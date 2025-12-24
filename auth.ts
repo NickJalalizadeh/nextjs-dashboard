39ef2f85dff97820ca12095153e332a0cb861c6b
Nick Jalalizadeh <nick.jalalizadeh@gmail.com>
2025-12-23 17:26:14 -0800

    Commented out auth code to test deployment issue

--- auth.ts
+++ auth.ts
@@ -8,15 +8,15 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
+// async function getUser(email: string): Promise<User | undefined> {
+//   try {
+//     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+//     return user[0];
+//   } catch (error) {
+//     console.error('Failed to fetch user:', error);
+//     throw new Error('Failed to fetch user.');
+//   }
+// }
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -28,13 +28,13 @@ export const { auth, signIn, signOut } = NextAuth({
 
       if (parsedCredentials.success) {
         const { email, password } = parsedCredentials.data;
-        const user = await getUser(email);
-        if (!user)
-          return null;
+        // const user = await getUser(email);
+        // if (!user)
+        //   return null;
 
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-        if (passwordsMatch)
-          return user;
+        // const passwordsMatch = await bcrypt.compare(password, user.password);
+        // if (passwordsMatch)
+        //   return user;
       }
 
       console.log('Invalid credentials');
